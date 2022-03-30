2177099597cc0686dd19a6cb039ba65b8daa0766
QKRM <sjh@_kakao.com>
2022-03-30 22:06:21 +0900

    chap4~

--- chap4/kings_knight.py
+++ chap4/kings_knight.py
@@ -3,3 +3,14 @@ row_col = input()
 row = int(row_col[1])
 col = int(ord(row_col[0])-ord('a') + 1)
 
+ways = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+
+count = 0
+
+for way in ways:
+    after_move_row = row + way[0]
+    after_move_col = col + way[1]
+    if after_move_col > 0 and after_move_col <= 8 and after_move_row > 0 and after_move_row <= 8:
+        count += 1
+
+print(count)
